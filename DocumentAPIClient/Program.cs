748cf80f04ab9093ef433bdc14aec4d0f0d37122
Damien Aicheh <damienaicheh@microsoft.com>
2026-06-04 15:53:07 +0000

    feat: Add Kiota generation.

---
+++ DocumentAPIClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using DocumentAPI;
+using DocumentAPI.Client;
+using DocumentAPI.Client.Models;
+using Microsoft.Kiota.Abstractions.Authentication;
+using Microsoft.Kiota.Http.HttpClientLibrary;
+
+// API requires no authentication, so use the anonymous
+// authentication provider
+var authProvider = new AnonymousAuthenticationProvider();
+// Create request adapter using the HttpClient-based implementation
+var adapter = new HttpClientRequestAdapter(authProvider);
+// Create the API client
+var client = new DocumentAPIClient(adapter);
+
+try
+{
+    
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ERROR: {ex.Message}");
+    Console.WriteLine(ex.StackTrace);
+}
